739dd3274304dfd7d162febd6435b4bb4507dc6b
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2025-04-11 11:46:52 -0600

    video

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-        <video src="/carmarketvideo1.mkv" autoPlay muted loop />
+        <video src="/carmarketvideo1.mkv" autoPlay loop controls />
     </div>
   )
 }
