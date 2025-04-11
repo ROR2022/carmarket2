629f463a651850f5dc48054e74559867440542eb
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2025-04-11 11:43:50 -0600

    video

---
+++ app/video/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <video src="/carmarketvideo1.mkv" autoPlay muted loop />
+    </div>
+  )
+}
+
+export default page
